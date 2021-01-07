01f062c82a55a25e15c04ca540db207ed521e1d6
Caterina <danizio@outlook.it>
2021-01-07 16:08:57 +0100

    Impostazioni iniziali

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php snack</title>
+    <?php> 
+      include 'data.php';
+    <?>
+</head>
+<body>
+    <?php
+     $name = "Sara";
+    ?>
+    <h1>Ciao mi chiamo 
+       <?php echo $name; 
+       ?>
+    </h1>
+    <?>
+</body>
+</html>
