b5dec181de30aeeb1422a5634c364be0cc886ece
Caterina <danizio@outlook.it>
2021-01-07 17:19:14 +0100

    Esercizio

---
+++ data/data.php
@@ -0,0 +1,49 @@
+<!-- Partiamo da questo array di hotel.
+https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Avremo un file PHP con il nostro "database" e un file con tutta la logica. -->
+
+<?php
+
+$hotels = [
+
+    [
+        'Nome' => 'Hotel Belvedere',
+        'Descrizione' => 'Hotel Belvedere',
+        'Parcheggio' => true,
+        'Voto' => 4,
+        'Distanza dal centro' => 10.4
+    ],
+    [
+        'Nome' => 'Hotel Futuro',
+        'Descrizione' => 'Hotel Futuro',
+        'Parcheggio' => true,
+        'Voto' => 2,
+        'Distanza dal centro' => 2
+    ],
+    [
+        'Nome' => 'Hotel Rivamare',
+        'Descrizione' => 'Hotel Rivamare',
+        'Parcheggio' => false,
+        'Voto' => 1,
+        'Distanza dal centro' => 1
+    ],
+    [
+        'Nome' => 'Hotel Bellavista',
+        'Descrizione' => 'Hotel Bellavista',
+        'Parcheggio' => false,
+        'Voto' => 5,
+        'Distanza dal centro' => 5.5
+    ],
+    [
+        'Nome' => 'Hotel Milano',
+        'Descrizione' => 'Hotel Milano',
+        'Parcheggio' => true,
+        'Voto' => 2,
+        'Distanza dal centro' => 50
+    ]
+  ]
+    
+
+
+?>
