6e3f159395708b87d6342832fbf41129b204947f
Caterina <danizio@outlook.it>
2021-01-07 17:19:14 +0100

    Fix

--- index.php
+++ index.php
@@ -4,18 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php snack</title>
-    <?php> 
-      include 'data.php';
-    <?>
+    <link rel="stylesheet" href="css/style.css">
+    <?php
+      include 'data/data.php';
+    ?>
 </head>
 <body>
-    <?php
-     $name = "Sara";
-    ?>
-    <h1>Ciao mi chiamo 
-       <?php echo $name; 
-       ?>
-    </h1>
-    <?>
+  <div class="container">
+      <h1>Scopri i nostri migliori hotel</h1>
+        <ul class="flex">
+                <?php
+                    foreach ($hotels as $key => $values) {
+                ?>
+                <li>
+                    <?php
+                        foreach ($values as $data => $text) {
+                            if ($data === true) {
+                                $text = "Sì";
+                            } else if ($data === false) {
+                                $text = "No";
+                            };
+                            ?>
+                                <h2><?php echo $data .":" . " " . $text ?></h2>
+                            <?php
+                        }
+                    ?>
+                </li>
+            <?php
+            }
+        ?>    
+        </ul>
+  </div>
 </body>
 </html>
+
